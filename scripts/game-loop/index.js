e495e0dcb8381aa6d8e484c2c89255879a0c0fd7
CatVine <catriona.vine@neonwavestudios.co.uk>
2024-04-20 00:04:23 +0100

    Increase love on pet click and load from storage on init

--- scripts/game-loop/index.js
+++ scripts/game-loop/index.js
@@ -1,6 +1,7 @@
 import { petSelect, setPetSprite } from "../pet-select/index.js";
 import { checkForDataProperty } from "../utils/index.js";
 import { hideElement, showElement } from "../utils/index.js";
+import { increaseLove, setLoveTotal } from "../love-counter/index.js";
 
 export const startGameLoop = () => {
 
@@ -8,12 +9,20 @@ export const startGameLoop = () => {
   const localData = window.localStorage;
   const mainGameWindow = document.querySelector('#main-game');
   const petImageContainer = document.querySelector(".game__sprite");
+  const loveCounter = document.querySelector('.game__love-value');
 
   // Handle choosing a pet if there is no existing save data
   if (!checkForDataProperty('petChoice')) {
     petSelect(petImageContainer, mainGameWindow);
   } else {
     setPetSprite(petImageContainer);
+    setLoveTotal(localData.loveTotal, loveCounter);
     showElement(mainGameWindow);
   }
+
+  // Set pet love event
+
+  petImageContainer.addEventListener('click', () => { 
+    increaseLove(10, loveCounter)
+  })
 };
---
+++ scripts/love-counter/index.js
@@ -0,0 +1,11 @@
+export const increaseLove = (amount, counterElement) => {
+    let currentLoveTotal = Number(counterElement.textContent);
+    currentLoveTotal = currentLoveTotal + amount;
+
+    localStorage.setItem('loveTotal', currentLoveTotal);
+    setLoveTotal(currentLoveTotal, counterElement)
+}
+
+export const setLoveTotal = (total, element) => {
+    element.textContent = total;
+}
