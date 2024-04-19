e41c052e39d80c4cac289b95e05014a91e40ccd2
CatVine <catriona.vine@neonwavestudios.co.uk>
2024-04-19 23:46:10 +0100

    Add pet selection feature

--- scripts/pet-select/index.js
+++ scripts/pet-select/index.js
@@ -1,3 +1,23 @@
-export const petSelect = () => {
-    
+import { showElement, hideElement } from "../utils/index.js";
+
+export const petSelect = (petImageContainer, mainGameWindow) => {
+    const selectScreen = document.querySelector('#petSelect');
+    showElement(selectScreen);
+
+    const options = [...document.querySelectorAll('.game__option-button')];
+
+    options.forEach((option) => {
+        option.addEventListener('click', function () {
+            const petImage = option.querySelector('img');
+            localStorage.setItem('petChoice', petImage.src);
+
+            setPetSprite(petImageContainer);
+            hideElement(selectScreen);
+            showElement(mainGameWindow);
+        })
+    })
+}
+
+export const setPetSprite = (container) => {
+    container.src = localStorage.getItem("petChoice");
 }
