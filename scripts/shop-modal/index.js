a214231ad5fc59c7e88296aeca8520d6756a6430
CatVine <catriona.vine@neonwavestudios.co.uk>
2024-04-20 22:04:05 +0100

    Add tabbing

--- scripts/shop-modal/index.js
+++ scripts/shop-modal/index.js
@@ -6,4 +6,27 @@ export const handleShopModal = (modal, closeButton) => {
     } else {
         hideElement(modal);
     }
+
+    const shopTabButtons = [...modal.querySelectorAll('.modal__tab-button')];
+    const shopTabs = [...modal.querySelectorAll('.modal__tab-panel')];
+
+    shopTabButtons.forEach((button) => {
+        //find the associated tab panel from a data attribute on the button which points to the id of the panel
+        const associatedTabPanel = shopTabs.filter((tab) => tab.id === button.dataset.toggle )[0];
+
+        // when a button is clicked, switch the associated panel/tab to 'is--active' and deactivate the others
+        button.addEventListener('click', () => { switchTab(shopTabs, associatedTabPanel)})
+    })
+}
+
+export const switchTab = (tabs, tabPanel) => {
+    tabs.forEach((tab) => {
+        console.log(tab, tabPanel)
+        if (tab === tabPanel) {
+            showElement(tab);
+        } else {
+            hideElement(tab);
+        }
+        //change these functions to showElement, hideElement
+    })
 }
