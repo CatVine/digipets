82f80257b7f575cf3e656b6e15b1601aae6631fc
CatVine <catriona.vine@neonwavestudios.co.uk>
2024-04-25 22:30:18 +0100

    Fix accessibility

--- scripts/mobile-nav/index.js
+++ scripts/mobile-nav/index.js
@@ -3,8 +3,10 @@ export const toggleMobileNav = (element, toggleButton) => {
     if (element.classList.contains('is--active')) {
         element.classList.remove('is--active');
         toggleButton.ariaExpanded = false;
+        document.body.classList.remove('modal--open');
     } else {
         element.classList.add('is--active');
         toggleButton.ariaExpanded = true;
+        document.body.classList.add('modal--open');
     }
 }
